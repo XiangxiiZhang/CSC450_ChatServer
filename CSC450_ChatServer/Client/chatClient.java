b014d2e6e9ca08acd9cae66e47ea6286aa42fc1d
XiangxiiZhang <xiangxiizhang@outlook.com>
2022-04-25 15:44:22 -0700

    Second commit

--- CSC450_ChatServer/Client/chatClient.java
+++ CSC450_ChatServer/Client/chatClient.java
@@ -3,14 +3,34 @@ import java.util.Scanner;
 import java.io.PrintStream;
 public class chatClient
 {
-        public static void main(String[] args)
+    public static void main(String[] args)
+    {
+        Socket s = new Socket('localhost', 2222);
+        Scanner clientInput = new Scanner(client.getInputStream());
+        string question = clientInput.nextLine();
+        System.out.println(question);
+        Scanner localInput = new Scanner(system.in);
+        PrintStream clientOutput = new PrintStream(client.getOutputStream());
+        String myString = new String("Woot")
+
+
+        Thread lt = new Thread() { System.out.println("woot");};
+            public void madeUpMethod(){};
+            public void run()
+            {
+                String line;
+                while(true)
+                {
+                    line = clientInput.nextLine();
+                    System.out.println(line);
+                }
+            }
+        };
+        lt.start();
+        while(true)
         {
-            Socket s = new Socket('localhost', 2222);
-            Scanner clientInput = new Scanner(client.getInputStream());
-            string question = clientInput.nextLine();
-            System.out.println(question);
-            Scanner localInput = new Scanner(system.in);
-            PrintStream clientOutput = new PrintStream(client.getOutputStream());
-            clientOutput.printIn(localInput.nextline());
+            clientOutput. println(localInput.nextLine());
         }
+    
+    }
 }
--- CSC450_ChatServer/Server/chatServer.java
+++ CSC450_ChatServer/Server/chatServer.java
@@ -1,19 +1,21 @@
 import java.io.PrintStream;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.util.Random;
 public class chatServer
 {
     public static void main(String[] args) throws Exception
     {
         ServerSocket s = new ServerSocket(2222);
-        System.out.println("Listenning for Connection...");
-        Socket client = s.accept(); //blocks
-        System.out.println("Connection Established...");
-        PrintStream clientOutput = new PrintStream(client.getOutputStream());
-        Scanner clientInput = new Scanner(client.getInputStream());
-        clientOutput.println("What is your name?");
-        String name = clientInput.nextLine();
-        System.out.println("read: " + name);
+        ArrayList<ChatWorkerThread> theThreads = new ArrayList<ChatWorkerThread>();
+        while(true)
+        {
+            System.out.println("Listenning for Connection...");
+            Socket client = s.accept(); //blocks
+            ChatWorkerThread t = new ChatWorkerThread(client);
+            theThreads.add(t);
+            t.start();
+        }
     }
 }
